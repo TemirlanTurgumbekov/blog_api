18d0b5a294ce507019b33a6b71944a2024c6ad0d
tturgum5ekov <tturgum5ekov@gmail.com>
2023-06-16 20:53:41 +0600

    initial commit

---
+++ posts/serializers.py
@@ -0,0 +1,73 @@
+from rest_framework import serializers
+
+from category.models import Category
+from .models import Post, PostImages
+from comment.serializers import CommentSerializers
+
+
+class PostImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PostImages
+        fields = '__all__'
+
+
+class PostListSerializer(serializers.ModelSerializer):
+    owner_username = serializers.ReadOnlyField(source='owner.username')
+    category_name = serializers.ReadOnlyField(source='category.name')
+    images = PostImageSerializer(many=True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'owner_username', 'category_name', 'preview', 'images')
+
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['comments_count'] = instance.comments.count()
+        repr['likes_count'] = instance.likes.count()
+
+        user = self.context['request'].user
+        if user.is_authenticated:
+            repr['is_liked'] = user.likes.filter(post=instance).exists()
+            repr['is_favorite'] = user.favorites.filter(post=instance).exists()
+
+        return repr
+
+
+class PostCreateSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(required=True, queryset=Category.objects.all())
+    owner = serializers.ReadOnlyField(source='owner.id')
+    images = PostImageSerializer(many=True, required=False)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def create(self, valid_data):
+        request = self.context.get('request')
+        images = request.FILES.getlist('images')
+        post = Post.objects.create(**valid_data)
+        for image in images:
+            PostImages.objects.create(image=image, post=post)
+        return post
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    owner_username = serializers.ReadOnlyField(source='owner.username')
+    category_name = serializers.ReadOnlyField(source='category.name')
+    images = PostImageSerializer(many=True)
+    # comments = CommentSerializers(many=True)  # Способ #1
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        repr = super().to_representation(instance)
+        repr['comments_count'] = instance.comments.count()
+        repr['comments'] = CommentSerializers(instance.comments.all(), many=True).data
+        repr['like_count'] = instance.likes.count()
+        user = self.context['request'].user
+        if user.is_authenticated:
+            repr['is_liked'] = user.likes.filter(post=instance).exists()
+            repr['is_favorite'] = user.favorites.filter(post=instance).exists()
+        return repr
---
+++ posts/views.py
@@ -0,0 +1,102 @@
+from rest_framework import permissions
+from rest_framework.decorators import action
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+
+from comment.serializers import CommentSerializers
+from like.models import Favorite
+from like.serializers import LikeUserSerializer
+from .models import Post
+from . import serializers
+from .permissions import IsAuthor, IsAuthorOrAdmin
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
+
+class StandartResultPagination(PageNumberPagination):
+    page_size = 3
+    page_query_param = 'page'
+
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    pagination_class = StandartResultPagination
+    filter_backends = (DjangoFilterBackend, SearchFilter)
+    filterset_fields = ('owner', 'category', )
+    search_fields = ('title', 'body')
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return serializers.PostListSerializer
+        elif self.action in ('create', 'update', 'partial_update'):
+            return serializers.PostCreateSerializer
+        return serializers.PostDetailSerializer
+
+    def get_permissions(self):
+        if self.action == 'destroy':
+            return [IsAuthorOrAdmin(), ]
+        elif self.action in ('update', 'partial_update'):
+            return [IsAuthor(), ]
+        return [permissions.IsAuthenticatedOrReadOnly(), ]
+
+    @action(['GET'], detail=True)
+    def comments(self, request, pk):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = CommentSerializers(instance=comments, many=True)
+        return Response(serializer.data, status=200)
+
+    @action(['GET'], detail=True)
+    def likes(self, request, pk):
+        post = self.get_object()
+        likes = post.likes.all()
+        serializer = LikeUserSerializer(instance=likes, many=True)
+        return Response(serializer.data, status=200)
+
+    @action(['POST', 'DELETE'], detail=True)
+    def favorites(self, request, pk):
+        post = self.get_object()
+        user = request.user
+        favorite = user.favorites.filter(post=post)
+
+        if request.method == 'POST':
+            if favorite.exists():
+                return Response({'msg': 'Already in Favorite Favorite'}, status=400)
+            Favorite.objects.create(owner=user, post=post)
+            return Response({'msg': 'Added to Favorites'}, status=201)
+
+        if favorite.exists():
+            favorite.delete()
+            return Response({'msg': 'Deleted from Favorite'}, status=204)
+        return Response({'msg': 'Post Not Found in Favorite'}, status=404)
+
+# class PostListCreateView(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#
+#     def get_serializer_class(self):
+#         if self.request.method == 'GET':
+#             return serializers.PostListSerializer
+#         return serializers.PostCreateSerializer
+#
+#     def perform_create(self, serializer):
+#         serializer.save(owner=self.request.user)
+#
+#
+# class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#
+#     def get_serializer_class(self):
+#         if self.request.method in ('PUT', 'PATCH'):
+#             return serializers.PostCreateSerializer
+#         return serializers.PostDetailSerializer
+#
+#     def get_permissions(self):
+#         if self.request.method in ('PUT', 'PATCH'):
+#             return [IsAuthor()]
+#         elif self.request.method == 'DELETE':
+#             return [IsAuthorOrAdmin()]
+#         return [permissions.AllowAny()]
